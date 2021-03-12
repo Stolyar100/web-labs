f4e7ce2fd881d557651b8b7567fd20cf96b9b472
Stolyar100 <olehmarchukdev@gmail.com>
2021-03-12 18:18:58 +0200

    set React as Express view engine

--- lab1/src/app.js
+++ lab1/src/app.js
@@ -1,10 +1,23 @@
 import express from 'express'
+import expressReactVeiws from 'express-react-views'
+import path from 'path'
 
 const PORT = process.env.PORT || 3200
+
 const app = express()
+app.set('views', path.resolve('src/views'))
+app.set('view engine', 'jsx')
+app.engine('jsx', expressReactVeiws.createEngine())
 
 app.get('/', (req, res) => {
-  res.send("<h1>Hello World!!!</h1>")
+  res.render('Lab1', { studentName: 'Олег Марчук', variant: '19' })
 })
 
-app.listen(PORT, () => console.log('\x1b[32m', `Server is running on port ${PORT}...`))
+app.listen(PORT, () =>
+  console.log(
+    '\x1b[32m',
+    'Server is running on',
+    '\x1b[37m',
+    `http://localhost:${PORT}`
+  )
+)
